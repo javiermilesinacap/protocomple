3485fce0255fd4d77df93b22b722e83c213a5daa
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2024-06-03 13:31:05 -0400

    se agrega producto

--- proytocomple/app/page.js
+++ proytocomple/app/page.js
@@ -1,10 +1,24 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Image from 'next/image';
 export default function Home() {
   return (
     <main className="mx-auto max-w-6xl p-4">
       <Header />
       <Hero />
+      <div>
+        Productos
+        <div className='grid grid-cols-3 gap-4'>
+            <div className='bg-slate-100 rounded-lg text-center'>
+              <div className='flex justify-center p-4' >
+              <Image className='object-scale-down' src={'/assets/img/mockups/product1.jpg'}
+              width={100} height={100} alt='Producto' />
+              </div>
+              <h4 className='font-semimbold'>EL PRODUCTO</h4>
+              <p>Producto de prueba, por medio de un mockup</p>
+            </div>
+        </div>
+      </div>
     </main>
   );
 }
