5d365ced5f6576b7d755835f8001b1e541fa34f8
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2024-06-04 16:10:16 -0400

    Añado json con datos

--- proytocomple/app/page.js
+++ proytocomple/app/page.js
@@ -1,9 +1,9 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Card from '@/components/Card'
-
+import products from '@/mockup/products.json'
 export default function Home() {
-  const productos = ["Producto","Producto","Producto","Producto","Producto"]
+  const productos = products
   return (
     <main className="mx-auto max-w-6xl p-4">
       <Header />
@@ -11,8 +11,8 @@ export default function Home() {
       <div>
         <h2 className='font-bold text-2xl text-center m-4'>Productos</h2>
         <div className='grid grid-cols-4 gap-4'>
-            {productos.map((element)=>{
-                return (<Card />)
+            {productos.data.map((element, index)=>{
+                return (<Card key={element.id} dato={element} />)
             })}
         </div>
       </div>
--- proytocomple/components/Card/index.js
+++ proytocomple/components/Card/index.js
@@ -1,15 +1,15 @@
 import Button from '@/components/Button'
 import Image from 'next/image';
-const index = () => {
+const index = ({ dato={} }) => {
     return (
         <div className='bg-slate-100 rounded-lg text-center hover:bg-slate-200'>
             <div className='flex justify-center p-4' >
-                <Image className='object-cover rounded-md' src={'/assets/img/mockups/product1.jpg'}
+                <Image className='object-cover rounded-md' src={'/assets/img/mockups/'+dato.imagen}
                     width={150} height={150} alt='Producto' />
             </div>
-            <h4 className='font-bold text-center'>EL PRODUCTO</h4>
-            <p>Producto de prueba, por medio de un mockup</p>
-            <p className='font-semibold scale-105'>$1520</p>
+            <h4 className='font-bold text-center'>{dato.nombre}</h4>
+            <p>{dato.descripcion}</p>
+            <p className='font-semibold scale-105'>${dato.precio}</p>
             <Button texto={'Add to Cart'} />
         </div>
     )
